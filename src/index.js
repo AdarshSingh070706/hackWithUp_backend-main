d4dfade0c22d562150e0ac877054d57e74b828bd
AdarshSingh070706 <adarshofficial2123@gmail.com>
2025-11-02 10:01:28 +0530

    ythg

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 // CORS Configuration - Only call once!
 const corsOptions = {
-  origin: ["https://hack-with-up-frontend.vercel.app/","http://localhost:5173"], // Your React frontend URL
+  origin: ["https://hack-with-up-frontend.vercel.app","http://localhost:5173"], // Your React frontend URL
   credentials: true, // Allow cookies
   sameSite: "strict",
   secure: true,
